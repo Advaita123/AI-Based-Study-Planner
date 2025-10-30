415ddeb2cff470b1116161ae0ca6c30293a14d98
Advaita <85452092+Advaita123@users.noreply.github.com>
2025-10-30 10:00:38 +0530

    Implement basic calculator functions and main logic

---
+++ main_calc.py
@@ -0,0 +1,35 @@
+def calculate(operation, a, b):
+  operations={
+    'add':add,
+    'subtract':subract,
+    'multiply':'multiply,
+    'divide':'divide
+    }
+
+def add(a,b):
+  return a+b
+  
+def subtract(a,b):
+  return a-b
+  
+def multiply(a,b):
+  return a*b
+  
+def divide(a,b):
+  if b==0:
+   raise ValueError("Cannot divide by zero")
+ return a / b
+
+if operation not in operations:
+  raise ValueError(f"{operation} not possible")
+def main():
+  print(f"2+2={calculate(add,2,2)}")
+  print(f"5-2={calculate(subtract,5,2)}")
+  print(f"2*3={calculate(multiply,2,3)}")
+  print(f"8/4={calculate(divide,8,4)}")
+return "All calculations completed!"
+
+if __name__ == "__main__":
+    result = main()
+    print(result)
+  
